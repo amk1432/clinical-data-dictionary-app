64e204884491eb8ea6a65aced28dc9772dae5109
Amith <mkamithkumar@gmail.com>
2019-10-12 14:46:51 +0530

    content alignment fix

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -13,11 +13,13 @@ function App() {
       <div className="container-fluid">
         <div className="row">
           <SideBar />
-          <Switch>
-            <Route path="/form" exact component={Form} />
-            <Route path="/contributor" exact component={Contributor} />
-            <Route path="/admin" exact component={Admin} />
-          </Switch>
+          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+            <Switch>
+              <Route path="/form" exact component={Form} />
+              <Route path="/contributor" exact component={Contributor} />
+              <Route path="/admin" exact component={Admin} />
+            </Switch>
+          </main>
         </div>
       </div>
     </Router>
