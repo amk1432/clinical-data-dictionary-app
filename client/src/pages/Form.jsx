b6c28bbd5925871f6d22667b1202ca8a9491c1dd
Amith <mkamithkumar@gmail.com>
2019-10-13 22:12:34 +0530

    form validation and style

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -18,13 +18,34 @@ class Form extends Component {
     }
 
     render() {
+        let showButton;
+        let noDataWarning;
+        if( this.state.variables.length > 0){
+            showButton =  <button type="button" className="btn btn-primary" >Submit</button>;
+           
+        }else if(this.state.variables.length === 0 ){
+            noDataWarning = <p>Please add variables via Admin console to see the form</p>;
+        }
         const formElem = this.state.variables.map((variable, key) =>
-            <input type="text" placeholder={variable.description} />
+            <div className="form-group col-md-6">
+                <label htmlFor="variableName">Variable Name</label>
+                <input type="text" className="form-control" placeholder={variable.description} />
+            </div>
         );
         return (
-            <form action="">
-                {formElem}
-            </form>
+            <div>
+                <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mt-3">
+                    <h1 className="h2">Clinical Data</h1>
+                          
+                </div>
+
+                <div className="form-row">
+                    {noDataWarning}
+                        {formElem}
+                       {showButton}
+                    
+                    </div>
+            </div>
         )
     }
 }
