f771bf4a8295002c902b15b006f062d75e52494a
Amith <mkamithkumar@gmail.com>
2019-10-13 21:40:21 +0530

    form initial commit

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import api from '../api';
 
 class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            variables: []
+        }
+    }
+
+    componentDidMount() {
+        api.getAllVariables().then(variables => {
+            this.setState({
+                variables: variables.data,
+            })
+        })
+    }
+
     render() {
+        const formElem = this.state.variables.map((variable, key) =>
+            <input type="text" placeholder={variable.description} />
+        );
         return (
-            <div>Form</div>
+            <form action="">
+                {formElem}
+            </form>
         )
     }
 }
