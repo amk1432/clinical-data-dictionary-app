edfe97a72aec434c77d2978e5772d449477415dd
Amith <mkamithkumar@gmail.com>
2019-10-13 22:21:16 +0530

    form control style

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -20,15 +20,17 @@ class Form extends Component {
     render() {
         let showButton;
         let noDataWarning;
-        if( this.state.variables.length > 0){
-            showButton =  <button type="button" className="btn btn-primary" >Submit</button>;
-           
-        }else if(this.state.variables.length === 0 ){
-            noDataWarning = <p>Please add variables via Admin console to see the form</p>;
+        if (this.state.variables.length > 0) {
+            showButton =<div className="form-group col-md-3">
+                <button type="button" className="btn btn-primary" >Submit</button>
+                </div>;
+
+        } else if (this.state.variables.length === 0) {
+            noDataWarning = <p>Please add variables via admin console to see the form.</p>;
         }
         const formElem = this.state.variables.map((variable, key) =>
-            <div className="form-group col-md-6">
-                <label htmlFor="variableName">Variable Name</label>
+            <div className="form-group col-md-3">
+                <label htmlFor="variableName">{variable.description}</label>
                 <input type="text" className="form-control" placeholder={variable.description} />
             </div>
         );
@@ -36,15 +38,13 @@ class Form extends Component {
             <div>
                 <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mt-3">
                     <h1 className="h2">Clinical Data</h1>
-                          
                 </div>
-
-                <div className="form-row">
-                    {noDataWarning}
-                        {formElem}
-                       {showButton}
+                {noDataWarning}
+                <form>
                     
-                    </div>
+                    {formElem}
+                    {showButton}
+                </form>
             </div>
         )
     }
